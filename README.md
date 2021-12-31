909554057003e158cd488b9d5bc5da9980d642cb
lucastkacz <lucastkacz@gmail.com>
2021-12-31 13:08:11 -0300

    Modified README and logic

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# library
+# Library [Visit Website](https://lucastkacz.github.io/library/)
+
+A library app from the Javascript course in [The Odin Project](https://www.theodinproject.com/).
+
+Styles from [Pico.css](https://picocss.com/)
--- js/script.js
+++ js/script.js
@@ -1,4 +1,15 @@
 let myLibrary = [];
+const nameInput = document.querySelector("#name");
+const authorInput = document.querySelector("#author");
+const pagesInput = document.querySelector("#pages");
+const statusInput = document.querySelector("#status");
+
+const formInput = document.querySelector("form");
+
+formInput.addEventListener("submit", () => {
+    onSubmit();
+    clearForm();
+});
 
 class Book {
     constructor(name, author, pages, status) {
@@ -7,14 +18,53 @@ class Book {
         this.pages = pages;
         this.status = status;
     }
-
-    addToList() {
-        myLibrary.push(this);
-    }
-
-    readToggle() {
-        this.status = !this.status;
-    }
 }
 
-const harryPotter = new Book("Harry Potter", "JK Rowling", 645, true);
+function clearLibrary() {
+    myLibrary.length = 0;
+}
+
+function onSubmit() {
+    const book = new Book(nameInput.value, authorInput.value, pagesInput.value, statusInput.value);
+    myLibrary.push(book);
+    console.table(myLibrary);
+    updateTable();
+}
+function clearForm() {
+    nameInput.value = "";
+    authorInput.value = "";
+    pagesInput.value = "";
+}
+function updateTable() {
+    const tBody = document.querySelector("tbody");
+    tBody.textContent = "";
+    for (let i = 0; i < myLibrary.length; i++) {
+        const newRow = document.createElement("tr");
+        tBody.appendChild(newRow);
+
+        const newNumber = document.createElement("td");
+        newNumber.textContent = i + 1;
+        newRow.appendChild(newNumber);
+
+        const newTitle = document.createElement("td");
+        newTitle.textContent = myLibrary[i].name;
+        newRow.appendChild(newTitle);
+
+        const newAuthor = document.createElement("td");
+        newAuthor.textContent = myLibrary[i].author;
+        newRow.appendChild(newAuthor);
+
+        const newPages = document.createElement("td");
+        newPages.textContent = myLibrary[i].pages;
+        newRow.appendChild(newPages);
+
+        const newStatus = document.createElement("td");
+        newStatus.textContent = myLibrary[i].status;
+        newRow.appendChild(newStatus);
+
+        const newDelete = document.createElement("button");
+        newDelete.textContent = "Delete";
+        newDelete.setAttribute("id", "delete-button");
+        newRow.appendChild(newDelete);
+    }
+}
