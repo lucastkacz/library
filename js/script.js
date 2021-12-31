53212b83f0cc000ad2b4829c5533806e88502cab
lucastkacz <lucastkacz@gmail.com>
2021-12-31 18:22:13 -0300

    Finished project

--- js/script.js
+++ js/script.js
@@ -1,15 +1,4 @@
 let myLibrary = [];
-const nameInput = document.querySelector("#name");
-const authorInput = document.querySelector("#author");
-const pagesInput = document.querySelector("#pages");
-const statusInput = document.querySelector("#status");
-
-const formInput = document.querySelector("form");
-
-formInput.addEventListener("submit", () => {
-    onSubmit();
-    clearForm();
-});
 
 class Book {
     constructor(name, author, pages, status) {
@@ -20,14 +9,24 @@ class Book {
     }
 }
 
-function clearLibrary() {
-    myLibrary.length = 0;
-}
+const nameInput = document.querySelector("#name");
+const authorInput = document.querySelector("#author");
+const pagesInput = document.querySelector("#pages");
+const statusInput = document.querySelector("#status");
+
+// Submit button handler
+const formInput = document.querySelector("form");
+formInput.addEventListener("submit", () => {
+    onSubmit();
+    clearForm();
+});
+// Delete button handler
+const tableInput = document.querySelector("table");
+tableInput.addEventListener("click", deleteRow);
 
 function onSubmit() {
     const book = new Book(nameInput.value, authorInput.value, pagesInput.value, statusInput.value);
     myLibrary.push(book);
-    console.table(myLibrary);
     updateTable();
 }
 function clearForm() {
@@ -37,34 +36,34 @@ function clearForm() {
 }
 function updateTable() {
     const tBody = document.querySelector("tbody");
-    tBody.textContent = "";
+    // Clears the table with every function call. Maybe it's not the most efficient way
+    tBody.innerHTML = "";
+    // Loops through every item in myLibrary and adds it to the body of the table
     for (let i = 0; i < myLibrary.length; i++) {
-        const newRow = document.createElement("tr");
-        tBody.appendChild(newRow);
-
-        const newNumber = document.createElement("td");
-        newNumber.textContent = i + 1;
-        newRow.appendChild(newNumber);
-
-        const newTitle = document.createElement("td");
-        newTitle.textContent = myLibrary[i].name;
-        newRow.appendChild(newTitle);
-
-        const newAuthor = document.createElement("td");
-        newAuthor.textContent = myLibrary[i].author;
-        newRow.appendChild(newAuthor);
-
-        const newPages = document.createElement("td");
-        newPages.textContent = myLibrary[i].pages;
-        newRow.appendChild(newPages);
-
-        const newStatus = document.createElement("td");
-        newStatus.textContent = myLibrary[i].status;
-        newRow.appendChild(newStatus);
-
-        const newDelete = document.createElement("button");
-        newDelete.textContent = "Delete";
-        newDelete.setAttribute("id", "delete-button");
-        newRow.appendChild(newDelete);
+        tBody.innerHTML += `
+            <tr>
+                <td>${i + 1}</td>
+                <td>${myLibrary[i].name}</td>
+                <td>${myLibrary[i].author}</td>
+                <td>${myLibrary[i].pages}</td>
+                <td>${myLibrary[i].status}</td>
+                <td><button class="delete-button">Delete</button></td>
+            </tr>`;
     }
+}
+function deleteRow(event) {
+    // Since the event listener is on the whole table and not on the button, we need to make sure only the button deletes the row
+    if (!event.target.classList.contains("delete-button")) {
+        return;
+    }
+    // Access the button inside the table row. (dot) target returns the element that triggered the event
+    const button = event.target;
+    // Get the first value of the table row, which is the index
+    const bookIndex = Number(button.parentNode.parentNode.childNodes[1].innerHTML) - 1;
+    // Remove the row/book from the table
+    button.closest("tr").remove();
+    // Remove the book from the list
+    myLibrary.splice(bookIndex, 1);
+    // Update table because the indexes have been changed
+    updateTable();
 }
